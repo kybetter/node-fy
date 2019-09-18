9c41759fcb131d24bc12eb9509d6416080868840
zhaoliang <kybetter@163.com>
2019-09-18 19:52:57 +0800

    refactor: 更新api，优化代码

--- index.js
+++ index.js
@@ -1,101 +1,85 @@
 #! /usr/bin/env node
 
-const http = require('http');
 const qs = require('querystring');
 const crypto = require('crypto');
-const md5 = crypto.createHash('md5');
+const axios = require('axios');
+const chalk = require('chalk');
 
-const pObj = {
-  print(data) {
-    if (data !== '') {console.log(data)}
-    return this;
-  },
-  line() {console.log('---------------------------------')}
+const sha256 = crypto.createHash('sha256');
+const log = console.log;
+
+function truncate(q) {
+  var len = q.length;
+  if (len <= 20) return q;
+  return q.substring(0, 10) + len + q.substring(len - 10, len);
 }
-function print(data = '') {return pObj.print(data)}
 
-class Translate {
-  constructor() {
-    [,,...this.argv] = process.argv;
-    this.argsStr = this.argv.join(' ');
-    this.APP_KEY = '6352bac0dedbf9e7';
-    this.SEC_KEY = '2bHmZHOz68od40aqJCD8KmjvEA1ovAkg';
-    this.salt = Date.now();
+function fy() {
+  const [, , ...argv] = process.argv;
+  const salt = Date.now();
+  const curtime = Math.round(salt / 1000);
+  const argsStr = argv.join(' ');
+  const APP_KEY = '6352bac0dedbf9e7';
+  const SEC_KEY = '2bHmZHOz68od40aqJCD8KmjvEA1ovAkg';
+  const params = {
+    q: argsStr,
+    appKey: APP_KEY,
+    salt: salt,
+    from: 'auto',
+    to: 'auto',
+    signType: "v3",
+    sign: sha256.update(`${APP_KEY}${truncate(argsStr)}${salt}${curtime}${SEC_KEY}`).digest('hex'),
+    curtime,
   }
-  
-  composeQueryData() {
-    return qs.stringify({
-      q: this.argsStr,
-      appKey: this.APP_KEY,
-      salt: this.salt,
-      sign: md5.update(`${this.APP_KEY}${this.argsStr}${this.salt}${this.SEC_KEY}`).digest('hex'),
-      from: 'auto',
-      to: 'auto'
-    });
-  }
-
-  requestOptions() {
-    return {
-      hostname: 'openapi.youdao.com',
-      path: `/api?${this.composeQueryData()}`,
-      method: 'GET',
-    }
-  }
-
-  run() {
-    const req = http.request(this.requestOptions(), res => {
-      let bodyData = '';
-      res.on('data', chunk => {bodyData += chunk});
-
-      res.on('end', () => {
-        const data = JSON.parse(bodyData);
-        const {web, basic, translation = [], l} = data;
-    
-        print(this.argsStr).line();
-
-        if (l === 'EN2zh-CHS') {
-          if (basic) {
-            print(`英[${basic['uk-phonetic'] || '无'}]  美[${basic['us-phonetic'] || '无'}]`).line();
-            basic.explains.forEach(item => {print(item)})
-            print().line();
-            if (basic.wfs) {
-              let wf = [];
-              basic.wfs.forEach(item => {
-                wf.push(item.wf.name)
-                wf.push(item.wf.value)
-              })
-              print(`[${wf.join(' ')}]`).line();
-            }
-          } else {
-            print(translation.join()).line();
-          }
-          if (web) {
-            print('短语');
-            web.forEach(item => {
-              print(`${item.key}: ${item.value.join('，')}`);
+  axios.post(`https://openapi.youdao.com/api`, qs.stringify(params)).then(res => {
+    const data = res.data;
+    if (data.errorCode === '0') {
+      const { web, basic, translation = [], l } = data;
+      log(chalk.yellow(`"${argsStr}" 的翻译结果：`))
+      log('---------------------------------')
+      if (l === 'en2zh-CHS') {
+        if (basic) {
+          log(chalk.cyan('音标：'), `英[${basic['uk-phonetic'] || '无'}]  美[${basic['us-phonetic'] || '无'}]`);
+          log('---------------------------------')
+          log(chalk.cyan('基本解释：'))
+          basic.explains.forEach(item => { log(item) })
+          log('---------------------------------')
+          if (basic.wfs) {
+            let wf = '';
+            basic.wfs.forEach(item => {
+              wf += item.wf.name + '：' + item.wf.value + '；';
             })
-            print().line();
+            log(wf);
+            log('---------------------------------')
           }
         } else {
-          if (basic && basic.explains) {
-            print(basic.explains.join(', ')).line();
-          } else {
-            print(translation.join()).line();
-          }
-          if (web) {
-            print('短语');
-            web.forEach(item => {
-              print(`${item.key}: ${item.value.join(', ')}`);
-            })
-            print().line();
-          }
+          log(translation.join());
         }
-      });
-    }).on('error', e => {
-      print('错误: ' + e.message);
-    });
-    req.end();
-  }
+        if (web) {
+          log(chalk.cyan('相关翻译：'));
+          web.forEach(item => {
+            log(`${item.key}: ${item.value.join('，')}`);
+          })
+        }
+      } else {
+        if (basic && basic.explains) {
+          log(chalk.cyan('基本翻译：'));
+          log(basic.explains.join('，'));
+          log('---------------------------------')
+        } else {
+          log(translation.join());
+        }
+        if (web) {
+          log(chalk.cyan('相关翻译：'));
+          web.forEach(item => {
+            log(`${item.key}: ${item.value.join('，')}`);
+          })
+        }
+      }
+    }
+  }).catch(err => {
+    log(err);
+  })
 }
 
-(new Translate()).run();
+fy();
